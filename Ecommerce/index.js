ae2e59480dd1914ecfe7c7fde4a6714f5397414d
Swaraj Anand <swrjnnd@gmail.com>
2024-07-26 13:06:51 +0530

    Added error classes and some chnged in routes

--- Ecommerce/index.js
+++ Ecommerce/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
 const userRouter = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -7,11 +9,15 @@ const mongoose = require("mongoose");
 const PORT = 8080;
 
 app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+// console.log(process.env.DB_LINK)
 
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/product",productRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/Ecommerce")
+mongoose.connect(process.env.DB_LINK || "mongodb://127.0.0.1:27017/Ecommerce")
 .then(() => {
     console.log("DB Connected")
     app.listen(PORT, () => {
