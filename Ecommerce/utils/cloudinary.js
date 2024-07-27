ec5a599ba4024eb33d8ae84f367b2820cc7a73d6
Swaraj Anand <swrjnnd@gmail.com>
2024-07-27 12:32:49 +0530

    Configured cloudniary

---
+++ Ecommerce/utils/cloudinary.js
@@ -0,0 +1,30 @@
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudnary = async (filePath) => {
+    try {
+        if(!filePath) return null;
+
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: "auto",
+        })
+
+        console.log("File uploaded")
+        console.log(response.url);
+
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        return null;
+    }
+}
+
+
+module.exports = uploadOnCloudnary;
+
